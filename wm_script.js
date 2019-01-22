81d7e6bf05a1d170d906b92156d9ac616c818f4e
Juan Verduzco <jverduzco046@west-mec.org>
2019-01-22 16:50:51 -0700

    FInal Commit
    Ready for Review

--- wm_script.js
+++ wm_script.js
@@ -1,4 +1,4 @@
-/*
+ /*
    New Perspectives on HTML and CSS, 7th Edition
    Tutorial 7
    Case Problem 3
@@ -13,62 +13,62 @@
 
 */
 
-function updatePreview() {
-   formStyles=document.forms[0];
-   inputtext=formStyles.sampleText.value;
+ function updatePreview() {
+     formStyles = document.forms[0];
+     inputtext = formStyles.sampleText.value;
 
-   previewobject=document.getElementById("output");
-   previewobject.innerHTML=inputtext.replace(/\n/g,"<br />");
-   
+     previewobject = document.getElementById("output");
+     previewobject.innerHTML = inputtext.replace(/\n/g, "<br />");
 
-   ffi=formStyles.fontfamily.selectedIndex;
-   previewobject.style.fontFamily=formStyles.fontfamily.options[ffi].text;
 
-   fsi=formStyles.fontstyle.selectedIndex;
-   previewobject.style.fontStyle=formStyles.fontstyle.options[fsi].text;
+     ffi = formStyles.fontfamily.selectedIndex;
+     previewobject.style.fontFamily = formStyles.fontfamily.options[ffi].text;
 
-   fwi=formStyles.fontweight.selectedIndex;
-   previewobject.style.fontWeight=formStyles.fontweight.options[fwi].text;
+     fsi = formStyles.fontstyle.selectedIndex;
+     previewobject.style.fontStyle = formStyles.fontstyle.options[fsi].text;
 
-   tdi=formStyles.textdecoration.selectedIndex;
-   previewobject.style.textDecoration=formStyles.textdecoration.options[tdi].text;
+     fwi = formStyles.fontweight.selectedIndex;
+     previewobject.style.fontWeight = formStyles.fontweight.options[fwi].text;
 
-   tti=formStyles.texttransform.selectedIndex;
-   previewobject.style.textTransform=formStyles.texttransform.options[tti].text;
- 
-   fvi=formStyles.fontvariant.selectedIndex;
-   previewobject.style.fontVariant=formStyles.fontvariant.options[fvi].text;  
+     tdi = formStyles.textdecoration.selectedIndex;
+     previewobject.style.textDecoration = formStyles.textdecoration.options[tdi].text;
 
-   previewobject.style.color=formStyles.color.value;
-   previewobject.style.backgroundColor=formStyles.backgroundcolor.value;
+     tti = formStyles.texttransform.selectedIndex;
+     previewobject.style.textTransform = formStyles.texttransform.options[tti].text;
 
-   previewobject.style.fontSize=formStyles.fontsize.value + "px";
-   previewobject.style.letterSpacing=formStyles.letterspacing.value+"px";
-   previewobject.style.wordSpacing=formStyles.wordspacing.value+"px";
-   previewobject.style.lineHeight=formStyles.lineheight.value+"em";
-   previewobject.style.textIndent=formStyles.textindent.value+"px"; 
-}
+     fvi = formStyles.fontvariant.selectedIndex;
+     previewobject.style.fontVariant = formStyles.fontvariant.options[fvi].text;
 
-window.onload=function() {
-   formStyles=document.forms[0];
+     previewobject.style.color = formStyles.color.value;
+     previewobject.style.backgroundColor = formStyles.backgroundcolor.value;
 
-   formStyles.fontfamily.onchange=updatePreview;
-   formStyles.fontstyle.onchange=updatePreview;
-   formStyles.fontweight.onchange=updatePreview;
-   formStyles.textdecoration.onchange=updatePreview;
-   formStyles.texttransform.onchange=updatePreview;
-   formStyles.fontvariant.onchange=updatePreview;
-   formStyles.color.onchange=updatePreview;
-   formStyles.backgroundcolor.onchange=updatePreview;
-   formStyles.fontsize.onchange=updatePreview;
-   formStyles.letterspacing.onchange=updatePreview;
-   formStyles.wordspacing.onchange=updatePreview;
-   formStyles.lineheight.onchange=updatePreview;
-   formStyles.textindent.onchange=updatePreview;
-   formStyles.sampleBox.onchange=updatePreview;
+     previewobject.style.fontSize = formStyles.fontsize.value + "px";
+     previewobject.style.letterSpacing = formStyles.letterspacing.value + "px";
+     previewobject.style.wordSpacing = formStyles.wordspacing.value + "px";
+     previewobject.style.lineHeight = formStyles.lineheight.value + "em";
+     previewobject.style.textIndent = formStyles.textindent.value + "px";
+ }
 
-}
+ window.onload = function () {
+     formStyles = document.forms[0];
 
-function redo() {
-   window.location.reload();
-}
+     formStyles.fontfamily.onchange = updatePreview;
+     formStyles.fontstyle.onchange = updatePreview;
+     formStyles.fontweight.onchange = updatePreview;
+     formStyles.textdecoration.onchange = updatePreview;
+     formStyles.texttransform.onchange = updatePreview;
+     formStyles.fontvariant.onchange = updatePreview;
+     formStyles.color.onchange = updatePreview;
+     formStyles.backgroundcolor.onchange = updatePreview;
+     formStyles.fontsize.onchange = updatePreview;
+     formStyles.letterspacing.onchange = updatePreview;
+     formStyles.wordspacing.onchange = updatePreview;
+     formStyles.lineheight.onchange = updatePreview;
+     formStyles.textindent.onchange = updatePreview;
+     formStyles.sampleBox.onchange = updatePreview;
+
+ }
+
+ function redo() {
+     window.location.reload();
+ }
